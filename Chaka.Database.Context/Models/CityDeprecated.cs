2e0cf19485526da283738a4b603f0a354f8e7c50
MARTINEPC\martine <martine.putra@indocyber.co.id>
2019-05-29 14:26:58 +0700

    s

---
+++ Chaka.Database.Context/Models/CityDeprecated.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chaka.Database.Context.Models
+{
+    public partial class CityDeprecated
+    {
+        public CityDeprecated()
+        {
+            KppBranch = new HashSet<KppBranch>();
+            Location = new HashSet<Location>();
+        }
+
+        public int Id { get; set; }
+        public int StateId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal? MinimumSalary { get; set; }
+        public bool IsActive { get; set; }
+        public int BusinessGroupId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DelDate { get; set; }
+
+        public virtual State State { get; set; }
+        public virtual ICollection<KppBranch> KppBranch { get; set; }
+        public virtual ICollection<Location> Location { get; set; }
+    }
+}
---
+++ Chaka.Database.Context/Models/Province.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chaka.Database.Context.Models
+{
+    public partial class Province
+    {
+        public Province()
+        {
+            City = new HashSet<City>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public int CountryId { get; set; }
+        public int BusinessGroupId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DelDate { get; set; }
+
+        public virtual Country Country { get; set; }
+        public virtual ICollection<City> City { get; set; }
+    }
+}
